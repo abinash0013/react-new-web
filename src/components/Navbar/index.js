07e97693349da50ac79b266a6233eec57dd97fa7
Abinash <abinash261997@gmail.com>
2022-02-17 15:34:10 +0530

    work on navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavItem,
+    NavLinks,
+    NavMenu,
+    SignupBtn,
+    NavBtn,
+} from "./NavbarElements";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">dollo</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup">Sign Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <SignupBtn to="/signup">Sign Up</SignupBtn>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
 };
 
 export default Navbar;
