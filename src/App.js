2e4fa6438360e8dfe98a9d39fff97dfcdda7fe88
Abinash <abinash261997@gmail.com>
2022-02-17 19:13:08 +0530

    work on navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
     return (
         <Router>
-            <Navbar></Navbar>
+            <Sidebar />
+            <Navbar />
         </Router>
     );
 }
